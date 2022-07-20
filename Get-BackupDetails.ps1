ab8f8bffa6271d209d689f2b8c6daa265a6d33ed
Anthony Watherston <anwather@microsoft.com>
2022-07-20 15:07:50 +1000

    Added workbook

--- Get-BackupDetails.ps1
+++ Get-BackupDetails.ps1
@@ -1,6 +1,23 @@
+Param([string]$WorkspaceId)
+
+$subscriptionQuery = @'
+resourcecontainers
+| where type == "microsoft.resources/subscriptions"
+| project subscriptionId, name
+'@
+
+$subscriptionResults = Search-AzGraph -Query $subscriptionQuery -First 1000
+
+$subscriptionHash = @{}
+
+$subscriptionResults | Foreach-Object {
+    $subscriptionHash.Add($_.subscriptionId, $_.name)
+}
+
 $backupQuery = @'
 resources
 | where type == 'microsoft.compute/virtualmachines'
+| extend powerState = tostring(properties.extended.instanceView.powerState.displayStatus)
 | join kind = leftouter    (
 recoveryservicesresources
 | where type == 'microsoft.recoveryservices/vaults/backupfabrics/protectioncontainers/protecteditems'
@@ -14,10 +31,10 @@ resources
 | extend rsv = tostring(rsv)
 | project rsv,id) on rsv
 | extend backedUp = iif(rsv == '',"false","true")
-| project virtualMachineId=['id'], recoveryServicesVault = id1,backedUp
+| project virtualMachineId=['id'], recoveryServicesVault = id1,backedUp,environment=tags["Environment"],location, powerState
 '@
 
-$backupResults = Search-AzGraph -Query $backupQuery
+$backupResults = Search-AzGraph -Query $backupQuery -First 1000
 
 $vaultStorageTypes = @{}
 
@@ -30,9 +47,56 @@ $vaultStorageTypes = @{}
     $vaultStorageTypes.Add($_, $storageType)
 }
 
+$backupResults | ForEach-Object {
+    $_ | Add-Member -MemberType NoteProperty -Name VM_Subscription -Value $subscriptionHash[$_.virtualMachineId.Split("/")[2]] -Force
+    $_ | Add-Member -MemberType NoteProperty -Name VM_Name -Value $_.virtualMachineId.Split("/")[-1] -Force
+}
+
 $backupResults | Foreach-Object {
     if ($_.backedUp) {
-        $_ | Add-Member -MemberType NoteProperty -Name StorageType -Value $vaultStorageTypes[$_.recoveryServicesVault]
+        $_ | Add-Member -MemberType NoteProperty -Name StorageType -Value $vaultStorageTypes[$_.recoveryServicesVault] -Force
+        $_ | Add-Member -MemberType NoteProperty -Name RSV_Subscription -Value $subscriptionHash[$_.recoveryServicesVault.Split("/")[2]] -Force
     }
 }
+
+$storageQuery = @'
+AzureDiagnostics
+| where OperationName == 'StorageAssociation'
+| project StorageConsumedInMBs_s,TimeGenerated,VirtualMachine=strcat_array(split(ProtectedContainerUniqueId_s,";", 4),'')
+| extend StorageConsumedInGBs= round(todouble(StorageConsumedInMBs_s)/1024,2)
+| summarize StorageConsumedInGBs=avg(StorageConsumedInGBs) by bin(TimeGenerated,1d), VirtualMachine
+| project TimeGenerated,VirtualMachine,StorageConsumedInGBs
+'@
+
+$storageResults = Invoke-AzOperationalInsightsQuery -Query $storageQuery -WorkspaceId $workspaceId
+
+$storageHash = @{}
+
+($storageResults.Results -as [Array]) | Foreach-Object {
+    if (!($storageHash.ContainsKey($_.VirtualMachine))) {
+        $storageHash.Add($_.VirtualMachine, [double]$_.StorageConsumedInGBs)
+    }
+}
+
+function ValidateEnvironment {
+    Param($StorageType, $Environment)
+    $npEnvironments = @("Production", "Development", "Non Production", "Test", "Staging", "Lab (Sandpit)", "Lab (Playpen)", "Training", "Test/Staging", "Pre-Production", "Pre-Production DR", "Sandpit", "UAT", "Integration")
+    $prEnvironments = @("Production")
+    switch ($StorageType) {
+        "LocallyRedundant" { if ($Environment -notin $npEnvironments) { return $true } else { return $false } }
+        "GeoRedundant" { if ($Environment -notin $prEnvironments) { return $true } else { return $false } }
+        "ZoneRedundant" { if ($Environment -notin $prEnvironments) { return $true } else { return $false } }
+        default { return $true }
+    }
+}
+
+$backupResults | Foreach-Object {
+    if ($_.backedUp -eq "true") {
+        $_ | Add-Member -MemberType NoteProperty -Name StorageConsumedInGBs -Value $storageHash[$_.VM_Name] -Force
+        $_ | Add-Member -MemberType NoteProperty -Name Validate -Value (ValidateEnvironment -StorageType $_.StorageType -Environment $_.Environment) -Force
+    } 
+}
+
+
+$backupResults | Export-Csv output.csv -Force
 
